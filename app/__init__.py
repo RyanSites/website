4affef7ddd29de85ed76487cd6e84960bb0354dd
Ryan <ryanmsites@gmail.com>
2013-12-15 06:38:06 -0500

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -14,3 +14,13 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 # important, for some reason, that this stay down here
 from app import views, models
+
+
+import logging
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler('tmp/website.log', 'a', 1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+app.logger.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.info('website startup')
